e77fd95518dc44f8261550acb513873331f1acf2
ganesh babu <gansuv@gmail.com>
2017-09-06 17:53:54 +0530

    vendor products has been added

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -1,63 +1,54 @@
-<?php 
+<?php
  
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-
-use App\Http\Controllers\PagesController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use App\Http\Requests;
 use App\User as user;
 use App\Cities as cities;
-use App\Country as country;
 use App\media_sub_category as mediaSubCat;
+use App\media_subtype as mediaSubType;
 
+use App\products_categories as productCat;
 
-
-
-use Illuminate\Auth\Middleware\Authenticate;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-
+use Illuminate\Support\Facades\Facade;
 use Session;
 use Response;
-use View; 
+use View;
 
 use DB;
 use Validator;
 
-class VendorController extends Controller
-{
+use App\Country as country;
 
-     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-     protected $redirectTo = '/login';
-		
-	 public function __construct()
-	 {
-		  if($this->middleware('auth'))
-		  {
-		  }
-		  
-		  
-//		   $this->middleware($this->guestMiddleware(), ['except' => ['logout', 'getVerification', 'getVerificationError']]);
-		   //$this->middleware($this->guestMiddleware(), ['except' => 'logout']);
-	 }
-	
-	protected function redirectTo()
-	{
-		return '/path';
-	}        
-               
+
+class VendorController extends Controller 
+{
+        public function __construct(Response $response)
+	{            
+            $user = Auth::user();                          
+            
+            if(!($user))
+            {
+                echo " Ganesh Ganesh Ganesh "; 
+                return view('vendor.404');                 
+                exit;
+            }
+	}  
+        
         public function vendor_zone()
 	{
 		return view("vendor.vendor_zone");
 	}
-	public function user_register() 
-	{
-                                
-                $loggedInList 	= 	Auth::user()->email;
+	
+        public function user_register() 
+	{        
+                
+                $loggedInList           = 	Auth::user()->email;
                 $irstname 		=  	Auth::user()->firstName;
                 $user_id 		=  	Auth::user()->id;
                                                                 
@@ -66,31 +57,28 @@ class VendorController extends Controller
                 $posts = \App\country::all();
                 return view('vendor.user_register', ['users_info'=>$users,'posts' => $posts,'email_list'=>$loggedInList,'id'=>$user_id]);
 	}
-	
-	
-	
         public function user_register_submit(Request $request)
         {   		
                         $user_id    =  	Auth::user()->id;
                         $user = user::find($user_id);
-						
                         $user->media_type       =   $request->Type;                         
-                        $user->mobile_number    =   $request->mobileNumber;                 								  						 
-						$user->company_name     =   addslashes($request->company_name);                        
-						$user->website          =   addslashes($request->Co_Website);						
-                        $user->off_emailId      =   addslashes($request->co_email_id);						
-                        $user->land_phone       =   $request->Landline_Ph_No;						
-                        $user->location         =   $request->Location;      
+                        $user->mobile_number    =   $request->mobileNumber;            
+                        $user->company_name     =   addslashes($request->company_name);
+                        $user->website          =   addslashes($request->Co_Website);
+                        $user->off_emailId      =   addslashes($request->co_email_id);
+                        $user->land_phone       =   $request->Landline_Ph_No;
+                        $user->location         =   $request->Location;
+                        $user->current_address  =   addslashes($request->Address);
                         $user->current_off_status   =   $request->current_office_status;            
-						$user->current_address  =   addslashes($request->Address);
-						
+
                         $user->is_active=1;
-                        $user->save();                        
+                        $user->save();         
             $success = 1;                        
             return $success;
         }
         public function user_login(Request $request)
-        {
+        {            
+                echo "aaaaaa";
                 $users = new user();
                 
                 $data = $request->all();
@@ -107,22 +95,35 @@ class VendorController extends Controller
         }
         public function media_select_type(Request $request)
         {
-                $data = $request->json()->all();                
-                $Media_id = $data['id']; 
+            $data = $request->json()->all();                
+            $Media_id = $data['id']; 
                 
-                $results = mediaSubCat::
+            $results = mediaSubCat::
                   where("category_id","=",$Media_id)
                 ->get();    
-         
-                $lists = json_encode($results);
-                return $lists;
+                //return Response::json($results);
+            $lists = json_encode($results);
+            return $lists;
         }
         public function create_media_p()
-        {
-		   if (Auth::user())
-		   {
-            return view('vendor.create_media_p');
-			}
-			
+        {      
+            
+            //mediaSubType
+            $main_cats = \App\media_subtype::all();
+            return view('vendor.create_media_p',['maincategory'=>$main_cats]);           
+        }
+        public function product_select_type(Request $request)
+        {            
+           //print_r($request->json()->all());
+           $media_sub_id = $request->json()->all();
+           $mediaId = $media_sub_id['id']['id'];
+           
+           $results = productCat::
+                  where("media_sub_cat_id","=",$mediaId)
+                ->get();        
+           
+            //print_r($results); return false;
+            $lists = json_encode($results);
+            return $lists;
         }
 }
